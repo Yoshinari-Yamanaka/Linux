452c975d61cc14f3307e071144fa82b0cf5c3bd7
Yoshinari-Yamanaka <59109232+Yoshinari-Yamanaka@users.noreply.github.com>
2021-01-03 17:43:28 +0900

    Update private_subnet_to_Internet.sh

--- private_subnet_to_Internet.sh
+++ private_subnet_to_Internet.sh
@@ -19,3 +19,13 @@ ssh -R 2080:localhost:1080 -i ~/.ssh/key_pair.pem ec2-user@${private_instance_ip
 sudo chmod 777 /etc/yum.conf
 echo 'proxy=socks5://127.0.0.1:2080' >> /etc/yum.conf
 sudo chmod 700 /etc/yum.conf
+
+################################
+# On the Local Client like PC
+# Use "Local Port Forwarding"
+################################
+public_instance_global_ip=""
+ssh -i ~/.ssh/key_pair.pem ec2-user@${public_instance_global_ip} -L 8080:${private_instance_ip}:80
+
+# you can access private instance on your Browser like this
+# http://localhost:8080
