c87d967cb0d1e6a68bf2e67cf13cf13170dfb47f
Yoshinari-Yamanaka <59109232+Yoshinari-Yamanaka@users.noreply.github.com>
2021-01-03 15:56:58 +0900

    Update private_subnet_to_Internet.sh

--- private_subnet_to_Internet.sh
+++ private_subnet_to_Internet.sh
@@ -14,7 +14,7 @@ private_instance_ip=""
 
 ssh -i ~/.ssh/key_pair.pem ec2-user@${public_instance_ip} -D 1080
 
-ssh -$ 2080:localhost:1080 -i ~/.ssh/key_pair.pem ec2-user@${private_instance_ip}
+ssh -R 2080:localhost:1080 -i ~/.ssh/key_pair.pem ec2-user@${private_instance_ip}
 
 sudo chmod 777 /etc/yum.conf
 echo 'proxy=socks5://127.0.0.1:2080' >> /etc/yum.conf
