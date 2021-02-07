d76370df69b5fb309210865ba727f983c7f79d2f
Yoshinari-Yamanaka <59109232+Yoshinari-Yamanaka@users.noreply.github.com>
2021-02-07 17:46:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -196,7 +196,7 @@ fi
 if "${Golang_would_be_installed}" ; then
     cd ~
     wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
     echo "export PATH=${PATH}:/usr/local/go/bin" >> ~/.bashrc
     check_status $?
 fi
